de73166320ef65cb2abc69f0049100d3b302bc19
Caroline Goulart <goulartscaroline@gmail.com>
2020-05-07 02:23:45 -0300

    running tests

--- src/server.js
+++ src/server.js
@@ -1,38 +1,36 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
-const routes = require('./routes')
-const methodOverride = require('method-override')
+const express = require('express');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
+const methodOverride = require('method-override');
 
-// Heroku connection
 const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: true
 });
 
 client.connect(function(err, client, done){
-    console.log('err=', err);
-    console.log('client=', client);
+    console.log('ATTENTION err=', err);
+    console.log('ATTENTION client=', client);
     client.query(text, values).then(ret_cb).catch(err_cb);
 });
 
-// Application config:
-const server = express()
-server.use(express.urlencoded({ extended: true }))
+const server = express();
+server.use(express.urlencoded({ extended: true }));
 
-server.use(express.static('public'))
-server.use(methodOverride('_method')) // primeiro deve sobrescrever o method post com o metodo put, e depois 
-server.use(routes)
-server.set('view engine', 'njk')
+server.use(express.static('public'));
+server.use(methodOverride('_method')); 
+server.use(routes);
+server.set('view engine', 'njk');
 
 nunjucks.configure('src/app/views', {
     express: server,
     autoescape: false,
     noCache: true
-})
+});
 
-server.listen(process.env.PORT || 5000)
+server.listen(process.env.PORT || 5000);
 
 // Running locally:
 // const express = require('express')
